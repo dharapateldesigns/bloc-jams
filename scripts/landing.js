2e8e0f7e373458262997dff5be443d74a57b3f9f
Dhara Patel <dharapateldesigns@gmail.com>
2018-04-21 20:17:25 -0700

    Dom Scripting Events Checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
  // our JavaScript will go here
-                var animatePoints = function() {
-                    var points = document.getElementsByClassName('point');
+                var pointsArray = document.getElementsByClassName('point');
+ 
+                var animatePoints = function(points) {
  
                  var revealPoint = function(index) {
                             points[index].style.opacity = 1;
@@ -15,4 +16,22 @@
                 }
                  
             };
+
+            window.onload = function() {
+
+                 if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+                    }
+                    
+                var sellingPoints = document.getElementsByClassName('selling-points')[0];
+                var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+             
+                window.addEventListener('scroll', function(event) {
+                console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+                 if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+                }
+            });
+        }
+
 
