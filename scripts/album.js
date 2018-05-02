b1e4f47eeb74f4495c36d3d33305466d4c9ad137
Dhara Patel <dharapateldesigns@gmail.com>
2018-05-02 00:35:18 -0700

    dom scripting album view final assignment

--- scripts/album.js
+++ scripts/album.js
@@ -30,7 +30,6 @@
      ]
  };
 
-<<<<<<< HEAD
  var albumLana = {
      title: 'Born to Die',
      artist: 'Lana Del Rey',
@@ -46,25 +45,6 @@
      ]
  };
 
-=======
-
-  var albumLana = {
-     title: 'Born to Die',
-     artist: 'Lana Del Rey',
-     label: 'Unknown',
-     year: '2007',
-     albumArtUrl: 'assets/images/album_covers/lana.png',
-     songs: [
-         { title: 'Radio', duration: '1:01' },
-         { title: 'Blue Jeans', duration: '5:01' },
-         { title: 'Born to Die', duration: '3:21'},
-         { title: 'National Anthem', duration: '3:14' },
-         { title: 'Off to the Races', duration: '2:15'}
-     ]
- };
-
-
->>>>>>> dom-scripting-album-view
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -76,7 +56,6 @@
  
      return template;
  };
-<<<<<<< HEAD
 
  var albumTitle = document.getElementsByClassName('album-view-title')[0];
  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
@@ -87,21 +66,6 @@
  
 
  var setCurrentAlbum = function(album) {
-=======
- 
- var albumTitle = document.getElementsByClassName('album-view-title')[0];
- var albumArtist = document.getElementsByClassName('album-view-artist')[0];
- var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
- var albumImage = document.getElementsByClassName('album-cover-art')[0];
- var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-
-
-var setCurrentAlbum = function(album) {
-     // #1
-   
-     // #2
->>>>>>> dom-scripting-album-view
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -117,10 +81,8 @@ var setCurrentAlbum = function(album) {
  };
  
  window.onload = function() {
-
      setCurrentAlbum(albumPicasso);
 
-<<<<<<< HEAD
      var albums = [albumPicasso, albumMarconi, albumLana];
      var index = 1;
 
@@ -128,25 +90,9 @@ var setCurrentAlbum = function(album) {
         setCurrentAlbum(albums[index]);
         index ++;
         if (index == albums.length) {
-=======
-
- var albums = [albumPicasso, albumMarconi, albumLana];
- var index = 1;
-
-     albumImage.addEventListener("click", function(event) {
-        setCurrentAlbum(albums[index]);
-        index++;
-        if (index == album.length) {
->>>>>>> dom-scripting-album-view
             index = 0;
         }
 
      });
-<<<<<<< HEAD
 
  };
-=======
- };
-
-
->>>>>>> dom-scripting-album-view
