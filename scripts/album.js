982fc88bd306cc8eaa31e3d16213bdc3dbc423af
Dhara Patel <dharapateldesigns@gmail.com>
2018-05-12 13:46:26 -0700

    dom play assignment final

--- scripts/album.js
+++ scripts/album.js
@@ -80,6 +80,8 @@
      }
  };
 
+
+
 var clickHandler = function(targetElement) {
 
     var songItem = getSongItem(targetElement);  
@@ -151,3 +153,39 @@ window.onload = function() {
      });
 
  };
+
+var findParentByClassName = function(element, targetClass) {
+    if (element.parentElement == undefined) {
+        console.log('No parent found');
+        return;
+    }
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        if (currentParent.className == targetClass) {
+            return currentParent;
+        } else {
+            console.log('No parent found with that class name');
+        }
+    }
+};
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
