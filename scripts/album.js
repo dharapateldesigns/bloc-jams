7adb6c170164f38944a712eeb28affbb3585357b
Dhara Patel <dharapateldesigns@gmail.com>
2018-05-20 22:46:54 -0700

    jquery-play-pause3

--- scripts/album.js
+++ scripts/album.js
@@ -120,41 +120,6 @@
  };
 
 
-<<<<<<< HEAD
-
-
-<<<<<<< HEAD
-=======
-
-
-
-=======
->>>>>>> jquery-play-pause
-var clickHandler = function(targetElement) {
-
-    var songItem = getSongItem(targetElement);  
-
-      if (currentlyPlayingSong === null) {
-         songItem.innerHTML = pauseButtonTemplate;
-         currentlyPlayingSong = songItem.getAttribute('data-song-number');
-     
-          } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-         songItem.innerHTML = playButtonTemplate;
-         currentlyPlayingSong = null;
-        
-          } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-         songItem.innerHTML = pauseButtonTemplate;
-         currentlyPlayingSong = songItem.getAttribute('data-song-number');
-     }
-
- };
- 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-
->>>>>>> jquery-play-pause
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var currentlyPlayingSong = null;
