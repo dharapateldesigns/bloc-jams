4b413b36d35cfc0b387788532ca26d1599871749
Dhara Patel <dharapateldesigns@gmail.com>
2018-05-26 18:10:07 -0700

    add play pause

--- scripts/album.js
+++ scripts/album.js
@@ -101,6 +101,24 @@ var clickHandler = function(targetElement) {
      }
 
  };
+
+ var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
  
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
